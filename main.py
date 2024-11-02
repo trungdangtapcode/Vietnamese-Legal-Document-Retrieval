a4f85a575d7fbdbf514ca6af00d18b0b7b322e9a
trungdangtapcode <tcuong1000@gmail.com>
2024-11-03 04:34:50 +0700

    add api

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import fastapi
 import uvicorn
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import Response
+from fastapi import Request
+from utils import api_search
 
 app = fastapi.FastAPI()
-
-@app.get("/")
-def index():
-    return {"message": "Hello, World!"}
+URL_API_SEARCH = 'https://825b-35-227-86-192.ngrok-free.app/'
+api_search.URL_SERVER = URL_API_SEARCH
 
 templates = Jinja2Templates(directory="templates")
 
+@app.get("/")
+async def index():
+    return {"message": "Hello, World!"}
+
+@app.get("/home")
+async def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+@app.get("/test")
+async def test():
+    return api_search.text_seach("cơn bão",10)
+
+@app.get("/update")
+async def update(url_api_server: str|None = None):
+    if url_api_server:
+        global URL_API_SEARCH
+        URL_API_SEARCH = url_api_server
+    return {"message": "Update!"}
---
+++ utils/api_search.py
@@ -0,0 +1,34 @@
+import requests
+import pickle
+import codecs
+import json
+
+URL_SERVER = ""
+
+def text_seach(text: str, k: int):
+    headers = {
+        "ngrok-skip-browser-warning": "69420",
+    }
+    params = {
+        "text": text,
+        "k": k
+    }
+    response = requests.get(URL_SERVER+'/text/', headers=headers, params=params)
+    encoded = response.text
+    pickled = json.loads(encoded)   
+    unpickled = pickle.loads(codecs.decode(pickled.encode(), "base64"))    
+    return unpickled
+
+def cid_seach(text: str, k: int):
+    headers = {
+        "ngrok-skip-browser-warning": "69420",
+    }
+    params = {
+        "text": text,
+        "k": k
+    }
+    response = requests.get(URL_SERVER+'/cid/', headers=headers, params=params)
+    encoded = response.text
+    pickled = json.loads(encoded)   
+    unpickled = pickle.loads(codecs.decode(pickled.encode(), "base64"))    
+    return unpickled
